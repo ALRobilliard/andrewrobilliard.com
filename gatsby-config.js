c38c943aed5132f7cf3dddc25fdba44c83183975
Andrew Robilliard <robilliardnz@gmail.com>
2020-04-11 13:32:05 +1200

    Update bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Andrew Robilliard`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Andrew Robilliard`,
+      summary: `who lives and works in Auckland, New Zealand building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `alrobilliard`,
     },
   },
   plugins: [
