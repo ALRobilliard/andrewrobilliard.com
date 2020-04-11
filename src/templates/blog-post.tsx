110a5b921ea42662880513701df54fad5ab905ee
Andrew Robilliard <robilliardnz@gmail.com>
2020-04-11 15:21:26 +1200

    Move blog posts to /blogs directory

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -105,7 +105,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "dddd, DD MMMM, YYYY")
         description
       }
     }
