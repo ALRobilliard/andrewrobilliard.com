528fa26681d1c57b90e7a773e2a445fb93a4f225
Andrew Robilliard <robilliardnz@gmail.com>
2020-07-25 21:50:41 +1200

    Add footer divider

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -13,43 +13,48 @@ const Footer = ({ message }: Props) => {
   message = message || ""
 
   return (
-    <footer className="custom-footer">
-      <div className="message">{message}</div>
-      <div className="icon-group">
-        <a
-          href="https://github.com/ALRobilliard/andrewrobilliard.com"
-          title="Open-source on Github"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <img src={github} alt="GitHub" />
-        </a>
-        <a
-          href="https://www.gatsbyjs.org/"
-          title="Built with Gatsby"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <img src={gatsby} alt="Gatsby" />
-        </a>
-        <a
-          href="https://www.typescriptlang.org/"
-          title="Built with TypeScript"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <img src={typescript} alt="TypeScript" />
-        </a>
-        <a
-          href="https://aws.amazon.com/"
-          title="Hosted on AWS"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <img src={aws} alt="AWS" />
-        </a>
+    <div>
+      <div className="divider">
+        <span>//</span>
       </div>
-    </footer>
+      <footer className="custom-footer">
+        <div className="message">{message}</div>
+        <div className="icon-group">
+          <a
+            href="https://github.com/ALRobilliard/andrewrobilliard.com"
+            title="Open-source on Github"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="GitHub" />
+          </a>
+          <a
+            href="https://www.gatsbyjs.org/"
+            title="Built with Gatsby"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={gatsby} alt="Gatsby" />
+          </a>
+          <a
+            href="https://www.typescriptlang.org/"
+            title="Built with TypeScript"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={typescript} alt="TypeScript" />
+          </a>
+          <a
+            href="https://aws.amazon.com/"
+            title="Hosted on AWS"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={aws} alt="AWS" />
+          </a>
+        </div>
+      </footer>
+    </div>
   )
 }
 
