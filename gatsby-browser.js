ef6b422321835ad95acd7ecebaf00b9a0d9f7c0e
Andrew Robilliard <robilliardnz@gmail.com>
2024-11-25 16:44:13 +1300

    Fix react hydration bug https://github.com/gatsbyjs/gatsby/discussions/36232

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,16 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+import ReactDOM from 'react-dom';
 
 import "prismjs/themes/prism.css"
 import "./src/styles/global.css"
+
+const replaceHydrateFunction = () => {
+  return (element, container) => {
+    const root = ReactDOM.createRoot(container)
+    root.render(element)
+  }
+}
+
+export { replaceHydrateFunction }
